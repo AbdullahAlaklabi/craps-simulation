5205dc8658ae0f9b1eada5daea31307c50baaa97
AbdullahAlaklabi <alak6604@stthomas.edu>
2019-09-27 05:09:45 -0500

    HOMEWORK-2

---
+++ CrapsSimulation/test/HWtest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HWtest
+{
+
+	@Test
+	public void test1()
+	{
+		
+		assertEquals("Die.testStatic()", Die.testStatic());
+		Die test1 = new Die();
+	}
+
+    @Test
+   
+    public void test2()
+{
+	    assertEquals("CrookedDie1.testStatic()", CrookedDie1.testStatic());
+		Die test2 = new CrookedDie1();
+}
+
+}
